b9a5347503b64af5ed63ed3fa15dbb624a9c814d
Hunain Pervaiz <hunainpervaiz487@gmail.com>
2024-02-13 11:19:54 +0500

    delete request completed

--- src/Components/ListUser.jsx
+++ src/Components/ListUser.jsx
@@ -1,19 +1,34 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { FaEdit, FaTrashAlt } from "react-icons/fa";
+
 function ListUser() {
   const [user, setUser] = useState([]);
 
   const getUsers = async () => {
     const result = await axios.get("http://localhost/api/index.php");
-    console.log(result.data);
     setUser(result.data);
-    console.log("users are:" + user);
+
+  };
+
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete("http://localhost/api/index.php", {
+        data: { id } // Sending id in the request body
+      });
+      alert("User deleted successfully");
+      getUsers(); // Refresh the user list after deletion
+    } catch (error) {
+      alert("Error deleting user: " + error);
+    }
   };
 
   useEffect(() => {
     getUsers();
-  }, []);
+  },[]);
+
+  
 
   return (
     <div>
@@ -24,16 +39,25 @@ function ListUser() {
             <th scope="col">NAME</th>
             <th scope="col">EMAIL</th>
             <th scope="col">MOBILE</th>
+            <th scope="col">ACTION</th>
           </tr>
         </thead>
         <tbody>
           {user.map((itm, index) => {
             return (
               <tr key={index}>
-               
                 <td>{itm.name}</td>
                 <td>{itm.email}</td>
                 <td>{itm.mobile}</td>
+                <td>
+                  <Link to={`/edit/${itm.id}`}>
+                    <FaEdit />
+                  </Link>
+                  <FaTrashAlt
+                    onClick={() => handleDelete(itm.id)}
+                    style={{ marginLeft: "10px", cursor: "pointer" }}
+                  />
+                </td>
               </tr>
             );
           })}
